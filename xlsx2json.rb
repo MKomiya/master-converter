78dd543f1b13029147983cc040e7b14303d542d0
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-03-02 21:12:57 +0900

    add support multiple sheet convert

--- xlsx2json.rb
+++ xlsx2json.rb
@@ -11,20 +11,27 @@ end
 s.default_sheet = s.sheets.first
 
 headers = {}
-(s.first_column..s.last_column).each do |col|
-    headers[col] = s.cell(s.first_row, col)
+s.sheets.each do |sheet|
+    s.default_sheet = sheet
+    headers[sheet] = {}
+    (s.first_column..s.last_column).each do |col|
+        headers[sheet][col] = s.cell(s.first_row, col)
+    end
 end
 
 hash = {}
-hash[s.default_sheet] = []
-((s.first_row + 1)..s.last_row).each do |row|
-    row_data = {}
-    headers.keys.each do |col|
-        value = s.cell(row, col)
-        value = value.to_i if s.celltype(row,col) == :float && value.modulo(1) == 0.0
-        row_data[headers[col]] = value
+s.sheets.each do |sheet|
+    s.default_sheet = sheet
+    hash[sheet] = [] 
+    ((s.first_row + 1)..s.last_row).each do |row|
+        row_data = {}
+        headers[sheet].keys.each do |col|
+            value = s.cell(row, col)
+            value = value.to_i if s.celltype(row,col) == :float && value.modulo(1) == 0.0
+            row_data[headers[sheet][col]] = value
+        end
+        hash[sheet] << row_data
     end
-    hash[s.default_sheet] << row_data
 end
 
 puts hash.to_json
