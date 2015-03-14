7931f6ad5f3546d79318bc98f5757200298b2970
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-03-14 20:06:53 +0900

    improve it

--- xlsx2json.rb
+++ xlsx2json.rb
@@ -2,37 +2,39 @@ require 'roo'
 require 'json'
 
 begin
-    s = Roo::Excelx.new($*[0])
-    filename = File.basename($*[0], '.xlsx')
+  s = Roo::Excelx.new($ARGV[0])
+  filename = File.basename($ARGV[0], '.xlsx')
 rescue
-    STDERR.puts "Usage: ruby xlsx2json.rb excelx_file"
-    exit false
+  STDERR.puts 'Usage: ruby xlsx2json.rb excelx_file'
+  exit false
 end
 
 s.default_sheet = s.sheets.first
 
 headers = {}
 s.sheets.each do |sheet|
-    s.default_sheet = sheet
-    headers[sheet] = {}
-    (s.first_column..s.last_column).each do |col|
-        headers[sheet][col] = s.cell(s.first_row, col)
-    end
+  s.default_sheet = sheet
+  headers[sheet] = {}
+  (s.first_column..s.last_column).each do |col|
+    headers[sheet][col] = s.cell(s.first_row, col)
+  end
 end
 
 hash = {}
 s.sheets.each do |sheet|
-    s.default_sheet = sheet
-    hash[sheet] = [] 
-    ((s.first_row + 1)..s.last_row).each do |row|
-        row_data = {}
-        headers[sheet].keys.each do |col|
-            value = s.cell(row, col)
-            value = value.to_i if s.celltype(row,col) == :float && value.modulo(1) == 0.0
-            row_data[headers[sheet][col]] = value
-        end
-        hash[sheet] << row_data
+  s.default_sheet = sheet
+  hash[sheet] = []
+  ((s.first_row + 1)..s.last_row).each do |row|
+    row_data = {}
+    headers[sheet].keys.each do |col|
+      value = s.cell(row, col)
+      if s.celltype(row, col) == :float && value.modulo(1) == 0.0
+        value = value.to_i
+      end
+      row_data[headers[sheet][col]] = value
     end
+    hash[sheet] << row_data
+  end
 end
 
 puts filename
